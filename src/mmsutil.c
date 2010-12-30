42c235eedcfaab2eb60d546e8eb7f60fc8beff5c
Denis Kenzior <denkenz@gmail.com>
2010-12-30 00:21:13 -0600

    mmsutil: Add framework for parsing mms messages
    Plus implement a basic parser for notification-ind messages

--- src/mmsutil.c
+++ src/mmsutil.c
@@ -24,11 +24,19 @@
 #endif
 
 #include <string.h>
+#include <time.h>
 
 #include <glib.h>
 
 #include "wsputil.h"
 #include "mmsutil.h"
+
+typedef gboolean (*header_handler)(struct wsp_header_iter *, void *);
+
+enum header_flag {
+	HEADER_FLAG_MANDATORY =			1,
+	HEADER_FLAG_MARKED =			8,
+};
 
 enum mms_header {
 	MMS_HEADER_BCC =			0x01,
@@ -55,27 +63,13 @@ enum mms_header {
 	MMS_HEADER_SUBJECT =			0x16,
 	MMS_HEADER_TO =				0x17,
 	MMS_HEADER_TRANSACTION_ID =		0x18,
+	MMS_HEADER_INVALID =			0x80,
 };
 
-#define CHECK_WELL_KNOWN_HDR(hdr)			\
-	if (wsp_header_iter_next(iter) == FALSE)	\
-		return FALSE;				\
-							\
-	if (wsp_header_iter_get_hdr_type(iter) !=	\
-			WSP_HEADER_TYPE_WELL_KNOWN)	\
-		return FALSE;				\
-							\
-	p = wsp_header_iter_get_hdr(iter);		\
-							\
-	if ((p[0] & 0x7f) != hdr)			\
-		return FALSE				\
-
-static gboolean extract_short(struct wsp_header_iter *iter,
-				enum mms_header hdr, unsigned char *out)
+static gboolean extract_short(struct wsp_header_iter *iter, void *user)
 {
+	unsigned char *out = user;
 	const unsigned char *p;
-
-	CHECK_WELL_KNOWN_HDR(hdr);
 
 	if (wsp_header_iter_get_val_type(iter) != WSP_VALUE_TYPE_SHORT)
 		return FALSE;
@@ -86,14 +80,12 @@ static gboolean extract_short(struct wsp_header_iter *iter,
 	return TRUE;
 }
 
-static gboolean extract_text(struct wsp_header_iter *iter, enum mms_header hdr,
-					const char **out)
+static gboolean extract_text(struct wsp_header_iter *iter, void *user)
 {
+	char **out = user;
 	const unsigned char *p;
 	unsigned int l;
 	const char *text;
-
-	CHECK_WELL_KNOWN_HDR(hdr);
 
 	if (wsp_header_iter_get_val_type(iter) != WSP_VALUE_TYPE_TEXT)
 		return FALSE;
@@ -105,22 +97,324 @@ static gboolean extract_text(struct wsp_header_iter *iter, enum mms_header hdr,
 	if (text == NULL)
 		return FALSE;
 
-	*out = text;
+	*out = g_strdup(text);
 
 	return TRUE;
 }
+
+static gboolean extract_date(struct wsp_header_iter *iter, void *user)
+{
+	time_t *out = user;
+	const unsigned char *p;
+	unsigned int l;
+	unsigned int i;
+
+	if (wsp_header_iter_get_val_type(iter) != WSP_VALUE_TYPE_LONG)
+		return FALSE;
+
+	p = wsp_header_iter_get_val(iter);
+	l = wsp_header_iter_get_val_len(iter);
+
+	if (l > 4)
+		return FALSE;
+
+	for (i = 0, *out = 0; i < l; i++)
+		*out = *out << 8 | p[i];
+
+	/* It is possible to overflow time_t on 32 bit systems */
+	*out = *out & 0x7fffffff;
+
+	return TRUE;
+}
+
+static gboolean extract_absolute_relative_date(struct wsp_header_iter *iter,
+						void *user)
+{
+	time_t *out = user;
+	const unsigned char *p;
+	unsigned int l;
+	unsigned int i;
+	unsigned int seconds;
+
+	if (wsp_header_iter_get_val_type(iter) != WSP_VALUE_TYPE_LONG)
+		return FALSE;
+
+	p = wsp_header_iter_get_val(iter);
+	l = wsp_header_iter_get_val_len(iter);
+
+	if (l < 2 || l > 5)
+		return FALSE;
+
+	if (p[0] != 128 && p[0] != 129)
+		return FALSE;
+
+	for (i = 1, seconds = 0; i < l; i++)
+		seconds = seconds << 8 | p[i];
+
+	if (p[0] == 129) {
+		*out = time(NULL);
+		*out += seconds;
+	} else
+		*out = seconds;
+
+	/* It is possible to overflow time_t on 32 bit systems */
+	*out = *out & 0x7fffffff;
+
+	return TRUE;
+}
+
+static gboolean extract_from(struct wsp_header_iter *iter, void *user)
+{
+	char **out = user;
+	const unsigned char *p;
+	unsigned int l;
+	const char *text;
+
+	if (wsp_header_iter_get_val_type(iter) != WSP_VALUE_TYPE_LONG)
+		return FALSE;
+
+	p = wsp_header_iter_get_val(iter);
+	l = wsp_header_iter_get_val_len(iter);
+
+	if (p[0] != 128 && p[0] != 129)
+		return FALSE;
+
+	if (p[0] == 129) {
+		*out = NULL;
+		return TRUE;
+	}
+
+	text = wsp_decode_text(p + 1, l - 1, NULL);
+	if (text == NULL)
+		return FALSE;
+
+	*out = g_strdup(text);
+
+	return TRUE;
+}
+
+static gboolean extract_message_class(struct wsp_header_iter *iter, void *user)
+{
+	char **out = user;
+	const unsigned char *p;
+	unsigned int l;
+	const char *text;
+
+	if (wsp_header_iter_get_val_type(iter) == WSP_VALUE_TYPE_LONG)
+		return FALSE;
+
+	p = wsp_header_iter_get_val(iter);
+
+	if (wsp_header_iter_get_val_type(iter) == WSP_VALUE_TYPE_SHORT) {
+		switch (p[0]) {
+		case 128:
+			*out = g_strdup("Personal");
+			return TRUE;
+		case 129:
+			*out = g_strdup("Advertisement");
+			return TRUE;
+		case 130:
+			*out = g_strdup("Informational");
+			return TRUE;
+		case 131:
+			*out = g_strdup("Auto");
+			return TRUE;
+		default:
+			return FALSE;
+		}
+	}
+
+	l = wsp_header_iter_get_val_len(iter);
+
+	text = wsp_decode_token_text(p, l, NULL);
+	if (text == NULL)
+		return FALSE;
+
+	*out = g_strdup(text);
+
+	return TRUE;
+}
+
+static gboolean extract_unsigned(struct wsp_header_iter *iter, void *user)
+{
+	unsigned long *out = user;
+	const unsigned char *p;
+	unsigned int l;
+	unsigned int i;
+
+	if (wsp_header_iter_get_val_type(iter) != WSP_VALUE_TYPE_LONG)
+		return FALSE;
+
+	p = wsp_header_iter_get_val(iter);
+	l = wsp_header_iter_get_val_len(iter);
+
+	if (l > sizeof(unsigned long))
+		return FALSE;
+
+	for (i = 0, *out = 0; i < l; i++)
+		*out = *out << 8 | p[i];
+
+	return TRUE;
+}
+
+static header_handler handler_for_type(enum mms_header header)
+{
+	switch (header) {
+	case MMS_HEADER_BCC:
+		return extract_text;
+	case MMS_HEADER_CC:
+		return extract_text;
+	case MMS_HEADER_CONTENT_LOCATION:
+		return extract_text;
+	case MMS_HEADER_CONTENT_TYPE:
+		return extract_text;
+	case MMS_HEADER_DATE:
+		return extract_date;
+	case MMS_HEADER_DELIVERY_REPORT:
+		return NULL;
+	case MMS_HEADER_DELIVERY_TIME:
+		return extract_absolute_relative_date;
+	case MMS_HEADER_EXPIRY:
+		return extract_absolute_relative_date;
+	case MMS_HEADER_FROM:
+		return extract_from;
+	case MMS_HEADER_MESSAGE_CLASS:
+		return extract_message_class;
+	case MMS_HEADER_MESSAGE_ID:
+		return extract_text;
+	case MMS_HEADER_MESSAGE_SIZE:
+		return extract_unsigned;
+	case MMS_HEADER_PRIORITY:
+		return NULL;
+	case MMS_HEADER_READ_REPLY:
+		return NULL;
+	case MMS_HEADER_REPORT_ALLOWED:
+		return NULL;
+	case MMS_HEADER_RESPONSE_STATUS:
+		return NULL;
+	case MMS_HEADER_RESPONSE_TEXT:
+		return NULL;
+	case MMS_HEADER_SENDER_VISIBILITY:
+		return NULL;
+	case MMS_HEADER_STATUS:
+		return NULL;
+	case MMS_HEADER_SUBJECT:
+		return extract_text;
+	case MMS_HEADER_TO:
+		return extract_text;
+	default:
+		return NULL;
+	}
+}
+
+struct header_handler_entry {
+	int flags;
+	void *data;
+};
+
+static gboolean mms_parse_headers(struct wsp_header_iter *iter,
+					enum mms_header header, ...)
+{
+	struct header_handler_entry entries[MMS_HEADER_TRANSACTION_ID + 1];
+	va_list args;
+	const unsigned char *p;
+	int i;
+
+	memset(&entries, 0, sizeof(entries));
+
+	va_start(args, header);
+
+	while (header != MMS_HEADER_INVALID) {
+		entries[header].flags = va_arg(args, int);
+		entries[header].data = va_arg(args, void *);
+
+		header = va_arg(args, enum mms_header);
+	}
+
+	while (wsp_header_iter_next(iter)) {
+		unsigned char h;
+		header_handler handler;
+
+		/* Skip application headers */
+		if (wsp_header_iter_get_hdr_type(iter) !=
+				WSP_HEADER_TYPE_WELL_KNOWN)
+			continue;
+
+		p = wsp_header_iter_get_hdr(iter);
+		h = p[0] & 0x7f;
+
+		/* Unsupported header, skip */
+		if (entries[h].data == NULL)
+			continue;
+
+		/* Skip multiply present headers */
+		if (entries[h].flags & HEADER_FLAG_MARKED)
+			continue;
+
+		handler = handler_for_type(h);
+		if (handler == NULL)
+			return FALSE;
+
+		if (handler(iter, entries[h].data) == FALSE)
+			return FALSE;
+
+		/* Parse the header */
+		entries[p[0] & 0x7f].flags |= HEADER_FLAG_MARKED;
+	}
+
+	for (i = 0; i < MMS_HEADER_TRANSACTION_ID + 1; i++) {
+		if ((entries[i].flags & HEADER_FLAG_MANDATORY) &&
+				!(entries[i].flags & HEADER_FLAG_MARKED))
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+static gboolean decode_notification_ind(struct wsp_header_iter *iter,
+						struct mms_message *out)
+{
+	return mms_parse_headers(iter, MMS_HEADER_FROM,
+				0, &out->ni.from,
+				MMS_HEADER_SUBJECT,
+				0, &out->ni.subject,
+				MMS_HEADER_MESSAGE_CLASS,
+				HEADER_FLAG_MANDATORY, &out->ni.cls,
+				MMS_HEADER_MESSAGE_SIZE,
+				HEADER_FLAG_MANDATORY, &out->ni.size,
+				MMS_HEADER_EXPIRY,
+				HEADER_FLAG_MANDATORY, &out->ni.expiry,
+				MMS_HEADER_CONTENT_LOCATION,
+				HEADER_FLAG_MANDATORY, &out->ni.location,
+				MMS_HEADER_INVALID);
+}
+
+#define CHECK_WELL_KNOWN_HDR(hdr)			\
+	if (wsp_header_iter_next(&iter) == FALSE)	\
+		return FALSE;				\
+							\
+	if (wsp_header_iter_get_hdr_type(&iter) !=	\
+			WSP_HEADER_TYPE_WELL_KNOWN)	\
+		return FALSE;				\
+							\
+	p = wsp_header_iter_get_hdr(&iter);		\
+							\
+	if ((p[0] & 0x7f) != hdr)			\
+		return FALSE				\
 
 gboolean mms_message_decode(const unsigned char *pdu,
 				unsigned int len, struct mms_message *out)
 {
 	struct wsp_header_iter iter;
+	const unsigned char *p;
 	unsigned char octet;
-	const char *text;
 
 	memset(out, 0, sizeof(*out));
 	wsp_header_iter_init(&iter, pdu, len, WSP_HEADER_ITER_FLAG_REJECT_CP);
 
-	if (extract_short(&iter, MMS_HEADER_MESSAGE_TYPE, &octet) == FALSE)
+	CHECK_WELL_KNOWN_HDR(MMS_HEADER_MESSAGE_TYPE);
+
+	if (extract_short(&iter, &octet) == FALSE)
 		return FALSE;
 
 	if (octet < 128 || octet > 134)
@@ -128,17 +422,36 @@ gboolean mms_message_decode(const unsigned char *pdu,
 
 	out->type = octet;
 
-	if (extract_text(&iter, MMS_HEADER_TRANSACTION_ID, &text) == FALSE)
+	CHECK_WELL_KNOWN_HDR(MMS_HEADER_TRANSACTION_ID);
+
+	if (extract_text(&iter, &out->transaction_id) == FALSE)
 		return FALSE;
 
-	out->transaction_id = g_strdup(text);
+	CHECK_WELL_KNOWN_HDR(MMS_HEADER_MMS_VERSION);
 
-	if (extract_short(&iter, MMS_HEADER_MMS_VERSION, &octet) == FALSE)
+	if (extract_short(&iter, &octet) == FALSE)
 		return FALSE;
 
 	out->version = octet & 0x7f;
 
-	return TRUE;
+	switch (out->type) {
+	case MMS_MESSAGE_TYPE_SEND_REQ:
+		return FALSE;
+	case MMS_MESSAGE_TYPE_SEND_CONF:
+		return FALSE;
+	case MMS_MESSAGE_TYPE_NOTIFICATION_IND:
+		return decode_notification_ind(&iter, out);
+	case MMS_MESSAGE_TYPE_NOTIFYRESP_IND:
+		return FALSE;
+	case MMS_MESSAGE_TYPE_RETRIEVE_CONF:
+		return FALSE;
+	case MMS_MESSAGE_TYPE_ACKNOWLEDGE_IND:
+		return FALSE;
+	case MMS_MESSAGE_TYPE_DELIVERY_IND:
+		return FALSE;
+	}
+
+	return FALSE;
 }
 
 void mms_message_free(struct mms_message *msg)
