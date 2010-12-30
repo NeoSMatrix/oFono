dc3c8e2e68326834370f6c1b534a633e2ecc13fd
Denis Kenzior <denkenz@gmail.com>
2010-12-29 20:03:35 -0600

    mmsutil: Add parsing of common mms elements

--- src/mmsutil.c
+++ src/mmsutil.c
@@ -23,8 +23,11 @@
 #include <config.h>
 #endif
 
+#include <string.h>
+
 #include <glib.h>
 
+#include "wsputil.h"
 #include "mmsutil.h"
 
 enum mms_header {
@@ -54,8 +57,86 @@ enum mms_header {
 	MMS_HEADER_TRANSACTION_ID =		0x18,
 };
 
+#define CHECK_WELL_KNOWN_HDR(hdr)			\
+	if (wsp_header_iter_next(iter) == FALSE)	\
+		return FALSE;				\
+							\
+	if (wsp_header_iter_get_hdr_type(iter) !=	\
+			WSP_HEADER_TYPE_WELL_KNOWN)	\
+		return FALSE;				\
+							\
+	p = wsp_header_iter_get_hdr(iter);		\
+							\
+	if ((p[0] & 0x7f) != hdr)			\
+		return FALSE				\
+
+static gboolean extract_short(struct wsp_header_iter *iter,
+				enum mms_header hdr, unsigned char *out)
+{
+	const unsigned char *p;
+
+	CHECK_WELL_KNOWN_HDR(hdr);
+
+	if (wsp_header_iter_get_val_type(iter) != WSP_VALUE_TYPE_SHORT)
+		return FALSE;
+
+	p = wsp_header_iter_get_val(iter);
+	*out = p[0];
+
+	return TRUE;
+}
+
+static gboolean extract_text(struct wsp_header_iter *iter, enum mms_header hdr,
+					const char **out)
+{
+	const unsigned char *p;
+	unsigned int l;
+	const char *text;
+
+	CHECK_WELL_KNOWN_HDR(hdr);
+
+	if (wsp_header_iter_get_val_type(iter) != WSP_VALUE_TYPE_TEXT)
+		return FALSE;
+
+	p = wsp_header_iter_get_val(iter);
+	l = wsp_header_iter_get_val_len(iter);
+
+	text = wsp_decode_text(p, l, NULL);
+	if (text == NULL)
+		return FALSE;
+
+	*out = text;
+
+	return TRUE;
+}
+
 gboolean mms_decode(const unsigned char *pdu,
 				unsigned int len, struct mms *out)
 {
-	return FALSE;
+	struct wsp_header_iter iter;
+	unsigned char octet;
+	const char *text;
+
+	memset(out, 0, sizeof(*out));
+	wsp_header_iter_init(&iter, pdu, len, WSP_HEADER_ITER_FLAG_REJECT_CP);
+
+	if (extract_short(&iter, MMS_HEADER_MESSAGE_TYPE, &octet) == FALSE)
+		return FALSE;
+
+	if (octet < 128 || octet > 134)
+		return FALSE;
+
+	out->type = octet;
+
+	if (extract_text(&iter, MMS_HEADER_TRANSACTION_ID, &text) == FALSE)
+		return FALSE;
+
+	out->transaction_id = g_strdup(text);
+
+	if (extract_short(&iter, MMS_HEADER_MMS_VERSION, &octet) == FALSE)
+		return FALSE;
+
+	out->version = octet & 0x7f;
+
+	return TRUE;
 }
