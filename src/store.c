fc441c1c8880c9cd390200607b08889376c95ce0
Regis Merlino <regis.merlino@linux.intel.com>
2011-04-21 16:27:47 +0200

    store: Add function to create storage dirs

--- src/store.c
+++ src/store.c
@@ -27,6 +27,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
 #include <glib.h>
 
@@ -45,6 +47,43 @@ static const char *digest_to_str(const unsigned char *digest)
 	buf[MMS_SHA1_UUID_LEN * 2] = 0;
 
 	return buf;
+}
+
+static int create_dirs(const char *filename, const mode_t mode)
+{
+	struct stat st;
+	char *dir;
+	const char *prev, *next;
+	int err;
+
+	if (filename[0] != '/')
+		return -1;
+
+	err = stat(filename, &st);
+	if (!err && S_ISREG(st.st_mode))
+		return 0;
+
+	dir = g_try_malloc(strlen(filename) + 1);
+	if (dir == NULL)
+		return -1;
+
+	strcpy(dir, "/");
+
+	for (prev = filename; (next = strchr(prev + 1, '/')); prev = next) {
+		/* Skip consecutive '/' characters */
+		if (next - prev == 1)
+			continue;
+
+		strncat(dir, prev + 1, next - prev);
+
+		if (mkdir(dir, mode) == -1 && errno != EEXIST) {
+			g_free(dir);
+			return -1;
+		}
+	}
+
+	g_free(dir);
+	return 0;
 }
 
 const char *mms_store(unsigned char *pdu, unsigned int len)
@@ -82,6 +121,12 @@ const char *mms_store(unsigned char *pdu, unsigned int len)
 	g_string_append(pathname, uuid);
 
 	DBG("pathname %s", pathname->str);
+
+	if (create_dirs(pathname->str, S_IRUSR | S_IWUSR | S_IXUSR) != 0) {
+		mms_error("Failed to create path %s", pathname->str);
+		uuid = NULL;
+		goto done;
+	}
 
 	fd = open(pathname->str, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR);
 	if (fd < 0) {
