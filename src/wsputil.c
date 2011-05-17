d07c63a5e5d12be92e627352dc8eafaa2fb6414d
Denis Kenzior <denkenz@gmail.com>
2011-05-17 16:39:37 -0500

    wsputil: Refactor content-type decoding
    Well-Known content-type values are consecutive, so we can avoid
    expensive O(n) lookup and go for a direct lookup instead

--- src/wsputil.c
+++ src/wsputil.c
@@ -37,22 +37,86 @@ struct wsp_hex_str_entry {
 /*
  * http://www.wapforum.org/wina/wsp-content-type.htm
  */
-static const struct  wsp_hex_str_entry content_types[] = {
-	{ 0x1C, "image/*" },
-	{ 0x1D, "image/gif" },
-	{ 0x1E, "image/jpeg" },
-	{ 0x1F, "image/tiff" },
-	{ 0x20, "image/png" },
-	{ 0x33, "application/vnd.wap.multipart.related" },
-	{ 0x36, "application/vnd.wap.connectivity-wbxml" },
-	{ 0x3E, "application/vnd.wap.mms-message" },
-	{ 0x44, "application/vnd.syncml.notification" },
-	{ 0x48, "application/vnd.oma.drm.message" },
-	{ 0x49, "application/vnd.oma.drm.content" },
-	{ 0x4A, "application/vnd.oma.drm.rights+xml" },
-	{ 0x4B, "application/vnd.oma.drm.rights+wbxml" },
-	{ 0xFF, NULL }
+static const char *content_types[] = {
+	"*/*",
+	"text/*",
+	"text/html",
+	"text/plain",
+	"text/x-hdml",
+	"text/x-ttml",
+	"text/x-vCalendar",
+	"text/x-vCard",
+	"text/vnd.wap.wml",
+	"text/vnd.wap.wmlscript",
+	"text/vnd.wap.wta-event",
+	"multipart/*",
+	"multipart/mixed",
+	"multipart/form-data",
+	"multipart/byterantes",
+	"multipart/alternative",
+	"application/*",
+	"application/java-vm",
+	"application/x-www-form-urlencoded",
+	"application/x-hdmlc",
+	"application/vnd.wap.wmlc",
+	"application/vnd.wap.wmlscriptc",
+	"application/vnd.wap.wta-eventc",
+	"application/vnd.wap.uaprof",
+	"application/vnd.wap.wtls-ca-certificate",
+	"application/vnd.wap.wtls-user-certificate",
+	"application/x-x509-ca-cert",
+	"application/x-x509-user-cert",
+	"image/*",
+	"image/gif",
+	"image/jpeg",
+	"image/tiff",
+	"image/png",
+	"image/vnd.wap.wbmp",
+	"application/vnd.wap.multipart.*",
+	"application/vnd.wap.multipart.mixed",
+	"application/vnd.wap.multipart.form-data",
+	"application/vnd.wap.multipart.byteranges",
+	"application/vnd.wap.multipart.alternative",
+	"application/xml",
+	"text/xml",
+	"application/vnd.wap.wbxml",
+	"application/x-x968-cross-cert",
+	"application/x-x968-ca-cert",
+	"application/x-x968-user-cert",
+	"text/vnd.wap.si",
+	"application/vnd.wap.sic",
+	"text/vnd.wap.sl",
+	"application/vnd.wap.slc",
+	"text/vnd.wap.co",
+	"application/vnd.wap.coc",
+	"application/vnd.wap.multipart.related",
+	"application/vnd.wap.sia",
+	"text/vnd.wap.connectivity-xml",
+	"application/vnd.wap.connectivity-wbxml",
+	"application/pkcs7-mime",
+	"application/vnd.wap.hashed-certificate",
+	"application/vnd.wap.signed-certificate",
+	"application/vnd.wap.cert-response",
+	"application/xhtml+xml",
+	"application/wml+xml",
+	"text/css",
+	"application/vnd.wap.mms-message",
+	"application/vnd.wap.rollover-certificate",
+	"application/vnd.wap.locc+wbxml",
+	"application/vnd.wap.loc+xml",
+	"application/vnd.syncml.dm+wbxml",
+	"application/vnd.syncml.dm+xml",
+	"application/vnd.syncml.notification",
+	"application/vnd.wap.xhtml+xml",
+	"application/vnd.wv.csp.cir",
+	"application/vnd.oma.dd+xml",
+	"application/vnd.oma.drm.message",
+	"application/vnd.oma.drm.content",
+	"application/vnd.oma.drm.rights+xml",
+	"application/vnd.oma.drm.rights+wbxml",
 };
+
+#define LAST_CONTENT_TYPE (sizeof(content_types) / sizeof(const char *))
 
 /*
  * http://www.wapforum.org/wina/push-app-id.htm
@@ -296,7 +360,10 @@ gboolean wsp_decode_content_type(const unsigned char *pdu, unsigned int max,
 
 		val = *pdu_val & 0x7f;
 
-		data = get_text_entry(val, content_types);
+		if (val > LAST_CONTENT_TYPE)
+			return FALSE;
+
+		data = content_types[val];
 	}
 
 	if (out_value)
