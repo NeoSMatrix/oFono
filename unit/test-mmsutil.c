1906ebe1cd86521b5ac83416e771f60a4601fc3d
Christophe Guiraud <christophe.guiraud@linux.intel.com>
2011-06-03 16:43:31 +0200

    unit: Add rc-mms-1-3-con-281.mms decoding test

--- unit/test-mmsutil.c
+++ unit/test-mmsutil.c
@@ -608,6 +608,25 @@ static const char mms_m_retrieve_conf_13[] = "./rc-mms-1-3-con-211.mms";
 static const char mms_m_retrieve_conf_14[] = "./rc-mms-1-3-con-282.mms";
 
 /*
+ * MMS M-Retrieve.Conf PDU 15
+ * MMS-1.3-con-281: Receive unrecognised header field.
+ * This PDU shows the decoding of a M-Retrieve.Conf PDU with an unrecognised
+ * header field (X-MMS-Unrecognised-Header-Field: "Yes") and below content:
+ * Overall message size: 6250
+ * MMS message type: retrieve-conf
+ * MMS transaction id: 00000000281
+ * MMS version: 1.3
+ * From: +33622222222/TYPE=PLMN
+ * To: +33666565565/TYPE=PLMN
+ * Subject: MMS-1.3-con-281
+ * Class: (null)
+ * Priority: (null)
+ * Msg-Id: (null)
+ * Date: 2011-06-03T16:37:02+0200
+ */
+static const char mms_m_retrieve_conf_15[] = "./rc-mms-1-3-con-281.mms";
+
+/*
  * MMS M-Send.Conf PDU 1
  * This PDU shows the decoding of a M-Send.Conf PDU with an accepted "response
  * status" and a below content:
@@ -722,6 +741,10 @@ static const struct mms_test mms_m_retrieve_conf_test_13 = {
 
 static const struct mms_test mms_m_retrieve_conf_test_14 = {
 	.pathname = mms_m_retrieve_conf_14,
+};
+
+static const struct mms_test mms_m_retrieve_conf_test_15 = {
+	.pathname = mms_m_retrieve_conf_15,
 };
 
 static const struct mms_test mms_m_send_conf_test_1 = {
@@ -908,6 +931,8 @@ int main(int argc, char **argv)
 				&mms_m_retrieve_conf_test_13, test_decode_mms);
 	g_test_add_data_func("/mmsutil/Decode MMS M-Retrieve.Conf PDU 14",
 				&mms_m_retrieve_conf_test_14, test_decode_mms);
+	g_test_add_data_func("/mmsutil/Decode MMS M-Retrieve.Conf PDU 15",
+				&mms_m_retrieve_conf_test_15, test_decode_mms);
 
 	g_test_add_data_func("/mmsutil/Decode MMS M-Send.Conf PDU 1",
 				&mms_m_send_conf_test_1, test_decode_mms);
