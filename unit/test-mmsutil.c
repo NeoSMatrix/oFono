0d2d97ce8b97173b87ea688edcf9999adc974183
Christophe Guiraud <christophe.guiraud@linux.intel.com>
2011-06-03 15:43:30 +0200

    unit: Add rc-mms-1-3-con-224.mms decoding test

--- unit/test-mmsutil.c
+++ unit/test-mmsutil.c
@@ -516,9 +516,38 @@ static const char mms_m_retrieve_conf_9[] = "./rc-mms-1-3-con-216.mms";
  * MMS-1.3-con-220: GIF Image size 160x120.
  * This PDU shows the decoding of a M-Retrieve.Conf PDU with a 160x120 GIF87a
  * Image object and below content:
-
+ * Overall message size: 4460
+ * MMS message type: retrieve-conf
+ * MMS transaction id: 00000000220
+ * MMS version: 1.3
+ * From: +33622222222/TYPE=PLMN
+ * To: +33666565565/TYPE=PLMN
+ * Subject: MMS-1.3-con-220
+ * Class: (null)
+ * Priority: (null)
+ * Msg-Id: (null)
+ * Date: 2011-06-03T15:27:27+0200
  */
 static const char mms_m_retrieve_conf_10[] = "./rc-mms-1-3-con-220.mms";
+
+/*
+ * MMS M-Retrieve.Conf PDU 11
+ * MMS-1.3-con-224: Animated GIF Image size 160x120.
+ * This PDU shows the decoding of a M-Retrieve.Conf PDU with a 160x120 animated
+ * GIF87a Image object and below content:
+ * Overall message size: 4265
+ * MMS message type: retrieve-conf
+ * MMS transaction id: 00000000224
+ * MMS version: 1.3
+ * From: +33622222222/TYPE=PLMN
+ * To: +33666565565/TYPE=PLMN
+ * Subject: MMS-1.3-con-224
+ * Class: (null)
+ * Priority: (null)
+ * Msg-Id: (null)
+ * Date: 2011-06-03T15:38:00+0200
+ */
+static const char mms_m_retrieve_conf_11[] = "./rc-mms-1-3-con-224.mms";
 
 /*
  * MMS M-Send.Conf PDU 1
@@ -619,6 +648,10 @@ static const struct mms_test mms_m_retrieve_conf_test_9 = {
 
 static const struct mms_test mms_m_retrieve_conf_test_10 = {
 	.pathname = mms_m_retrieve_conf_10,
+};
+
+static const struct mms_test mms_m_retrieve_conf_test_11 = {
+	.pathname = mms_m_retrieve_conf_11,
 };
 
 static const struct mms_test mms_m_send_conf_test_1 = {
@@ -797,6 +830,8 @@ int main(int argc, char **argv)
 				&mms_m_retrieve_conf_test_9, test_decode_mms);
 	g_test_add_data_func("/mmsutil/Decode MMS M-Retrieve.Conf PDU 10",
 				&mms_m_retrieve_conf_test_10, test_decode_mms);
+	g_test_add_data_func("/mmsutil/Decode MMS M-Retrieve.Conf PDU 11",
+				&mms_m_retrieve_conf_test_11, test_decode_mms);
 
 	g_test_add_data_func("/mmsutil/Decode MMS M-Send.Conf PDU 1",
 				&mms_m_send_conf_test_1, test_decode_mms);
