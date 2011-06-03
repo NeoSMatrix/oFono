f45fde48f781b3a9fe4fba8ed023c1a11b0d7124
Christophe Guiraud <christophe.guiraud@linux.intel.com>
2011-06-03 11:29:02 +0200

    unit: Add rc-mms-1-3-con-271.mms decoding test

--- unit/test-mmsutil.c
+++ unit/test-mmsutil.c
@@ -417,6 +417,25 @@ static const unsigned char mms_m_retrieve_conf_3[] = {
 static const char mms_m_retrieve_conf_4[] = "./rc-mms-1-3-con-210.mms";
 
 /*
+ * MMS M-Retrieve.Conf PDU 5
+ * MMS-1.3-con-271: Long Subject field.
+ * This PDU shows the decoding of a M-Retrieve.Conf PDU with a maximum length
+ * "us-ascii" encoded "subject" field value (40 characters) and below content:
+ * Overall message size: 556
+ * MMS message type: retrieve-conf
+ * MMS transaction id: 00000000271
+ * MMS version: 1.3
+ * From: +33622222222/TYPE=PLMN
+ * To: +33666565565/TYPE=PLMN
+ * Subject: abcdefghijklmnopqrstuvwxyz0123456789/-+@
+ * Class: (null)
+ * Priority: (null)
+ * Msg-Id: (null)
+ * Date: 2011-06-03T11:23:30+0200
+ */
+static const char mms_m_retrieve_conf_5[] = "./rc-mms-1-3-con-271.mms";
+
+/*
  * MMS M-Send.Conf PDU 1
  * This PDU shows the decoding of a M-Send.Conf PDU with an accepted "response
  * status" and a below content:
@@ -491,6 +510,10 @@ static const struct mms_test mms_m_retrieve_conf_test_3 = {
 
 static const struct mms_test mms_m_retrieve_conf_test_4 = {
 	.pathname = mms_m_retrieve_conf_4,
+};
+
+static const struct mms_test mms_m_retrieve_conf_test_5 = {
+	.pathname = mms_m_retrieve_conf_5,
 };
 
 static const struct mms_test mms_m_send_conf_test_1 = {
@@ -657,6 +680,8 @@ int main(int argc, char **argv)
 				&mms_m_retrieve_conf_test_3, test_decode_mms);
 	g_test_add_data_func("/mmsutil/Decode MMS M-Retrieve.Conf PDU 4",
 				&mms_m_retrieve_conf_test_4, test_decode_mms);
+	g_test_add_data_func("/mmsutil/Decode MMS M-Retrieve.Conf PDU 5",
+				&mms_m_retrieve_conf_test_5, test_decode_mms);
 
 	g_test_add_data_func("/mmsutil/Decode MMS M-Send.Conf PDU 1",
 				&mms_m_send_conf_test_1, test_decode_mms);
